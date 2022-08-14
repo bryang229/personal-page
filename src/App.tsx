55652eb5b9d160472d13b124fe9c8c618d6ede96
bryang229 <60994504+bryang229@users.noreply.github.com>
2022-08-14 00:49:30 -0400

    Added HPS ref!

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import logo from './logo.png';
 import radar from './images/radar.png';
 import bryan from './images/bryan.png';
@@ -10,33 +9,27 @@ import githubLogo from './images/github logo.png';
 import linkedInLogo from './images/linkedIn logo.png';
 import resume from './images/resume.png';
 import githubDarkMode from './images/github dark.png';
-import expand from './images/icons8down.png';
-import collapse from './images/icons8up.png'
-
+import ProjectElement from './ProjectElement';
 import './App.css';
+import React from 'react';
 
-
+let hpsDesc = `This is a Clock built during the HPS program under Google, using KICAD for creating the PCB's schema and then designing the PCB as well as exporting the footprints for production.\n
+This project was an amazing challenge, both in protyping designs and code using the Arduino, modules, and ICs such as the MAX7219 and the LCD module, but also in recreating these protypes in a schema.\n
+The Clock has two 8x8 Displays for showing time and three different modes ! \n
+The Rotary Encoder allows for changing the date as well as changing the mode of the display.\n
+From Binary Seconds to Decimals Seconds as well as a compact Hours Minutes Seconds mode (all in Binary!)\n
+For More info please click the demo link!
+`
 let radarDesc = `An arduino project which draws the location of nearby objects on a LED screen. It uses 4 Ultrasonic sensors to 
 detect nearby objects. Orginally designed for an arduino car made from an Elegoo kit. Below is a demo of the radar built using P5.js.`;
 let mathDesc = `A webapp orginally built during the All Star Code SI of 2021, the React Math App uses TypeScript and React for the front end.`;
 let musicDesc = `An app using react for the front end and Java with the spring framework for the backend. It uses spotify's api to get information of songs based user input`;
 let bryanDesc = `Hello and welcome to my website, a little introduction of myself. I am a student at New York Institute of Technology studying Electrical 
-and Computer Engineering. I have been a part of various programs such as the Google Code Next, Google Team Edge, and All Star Code SI. I am currently a Teaching
-Fellow for Emerging Leaders in Technology & Engineering (ELiTE)`;
+and Computer Engineering. I have been a part of various programs such as the Google Code Next, Google Team Edge, and All Star Code SI 2021. I've also been a Teaching Assistant for All Star Code SI 2022 under the NBA and Panasonic Cohorts as well as a Teaching Fellow for Emerging Leaders in Technology and Engineering (ELiTE)`;
 
 
-interface imageScale {
-    [index: number]: number;
-}
 
-interface projectProps {
-    title: string;
-    id: string;
-    preview: string;
-    link: string;
-    desc: string;
-    scale: imageScale;
-}
+
 
 const changeDisplayMode = (mode: boolean) => {
     // @ts-ignore
@@ -51,7 +44,8 @@ const Navbar = () => {
     let darkMode = false;
     return (
         <nav id={"navbar"}>
-            <img id={"logo"} alt={"logo"} width={70} height={70} src={logo}></img>
+            <a href = "/">
+            <img id={"logo"} alt={"logo"} width={70} height={70} src={logo}/></a>
             <div onClick={() => {
                 darkMode = !darkMode;
                 // @ts-ignore
@@ -93,65 +87,6 @@ const IntroToBryan = (props: BryanInfo) => {
 
 
 
-class ProjectElement extends React.Component<any, any>{
-    constructor(props: projectProps) {
-        super(props);
-
-        this.state = {
-            showOverview: true,
-            title: props.title,
-            id: props.id,
-            preview: props.preview,
-            link: props.link,
-            overview: props.desc.slice(0, 79) + "...",
-            desc: props.desc,
-            scale: props.scale
-        }
-    };
-
-    changeOverviewState = () => {
-        this.setState({
-            showOverview: !this.state.showOverview
-        })
-        return undefined;
-    }
-
-    render() {
-        if (this.state.showOverview) {
-            return (
-                <div className={"projects darkMode"} id={this.state.id}>
-                    <div onClick={this.changeOverviewState}>
-                        {this.state.title}
-                        <img height="15px" src={expand} />
-                    </div>
-                    <hr />
-                    <img src={this.state.preview} height={this.state.scale[0] / 2} width={this.state.scale[1] / 2}></img>
-                    <div id={this.state.id + "Overview"}>
-                        <p>{this.state.overview}</p>
-                    </div>
-                    <br />
-                    <div> <a href={this.state.link}>Try the demo here!</a> </div>
-
-                </div>);
-        }
-        return (
-            <div className={"projects darkMode"} id={this.state.id}>
-                <div onClick={this.changeOverviewState}>
-                    {this.state.title}
-                    <img height="15px" src={collapse}/>
-                </div>
-
-                <hr />
-                <img src={this.state.preview} height={this.state.scale[0]} width={this.state.scale[1]}></img>
-                <div id={this.state.id + "Desc"}>
-                    <p>{this.state.desc}</p>
-                </div>
-                <br />
-                <div> <a href={this.state.link}>Try the demo here!</a> </div>
-
-            </div>);
-    }
-}
 
 function App() {
 
@@ -161,6 +96,12 @@ function App() {
             <body className={"darkMode"}>
                 <IntroToBryan githubLogo={githubLogo} githubLink={"https://github.com/bryang229"} imgOfBryan={bryan} bryanDesc={bryanDesc}
                     linkedInLogo={linkedInLogo} linkedInLink={"https://linkedin.com/in/bryang229"} resumeImg={resume} />
+                <h2>Programs</h2>
+                <div id="programs">
+                    <ProjectElement title="Hardware Product Sprint" id="hps" preview={"https://github.com/bryang229/HPS-Work/blob/main/Clock%20CAD%20&%20Code/Clock/Clock%20raytracing.jpg?raw=true"} link="https://www.clock.bryang229.com/" 
+                    desc={hpsDesc} scale={[300, 400]} />
+                </div>
+
                 <h2>Projects:</h2>
                 <div id="projects">
                     <ProjectElement title="Radar" id="hardware" preview={radar} link="https://demo.bryang229.com" desc={radarDesc} scale={[300, 350]} />
---
+++ src/ProjectElement.tsx
@@ -0,0 +1,79 @@
+import expand from './images/icons8down.png';
+import collapse from './images/icons8up.png';
+import React from 'react';
+
+interface imageScale {
+    [index: number]: number;
+}
+
+interface projectProps {
+    title: string;
+    id: string;
+    preview: string;
+    link: string;
+    desc: string;
+    scale: imageScale;
+}
+
+
+class ProjectElement extends React.Component<any, any>{
+    constructor(props: projectProps) {
+        super(props);
+
+        this.state = {
+            showOverview: true,
+            title: props.title,
+            id: props.id,
+            preview: props.preview,
+            link: props.link,
+            overview: props.desc.slice(0, 79) + "...",
+            desc: props.desc,
+            scale: props.scale
+        }
+    };
+
+    changeOverviewState = () => {
+        this.setState({
+            showOverview: !this.state.showOverview
+        })
+        return undefined;
+    }
+
+    render() {
+        if (this.state.showOverview) {
+            return (
+                <div className={"projects darkMode"} id={this.state.id}>
+                    <div onClick={this.changeOverviewState}>
+                        {this.state.title}
+                        <img height="15px" src={expand} />
+                    </div>
+                    <hr />
+                    <img src={this.state.preview} height={this.state.scale[0] / 2} width={this.state.scale[1] / 2}></img>
+                    <div id={this.state.id + "Overview"}>
+                        <p>{this.state.overview}</p>
+                    </div>
+                    <br />
+                    <div> <a href={this.state.link}>Try the demo here!</a> </div>
+
+                </div>);
+        }
+        return (
+            <div className={"projects darkMode"} id={this.state.id}>
+                <div onClick={this.changeOverviewState}>
+                    {this.state.title}
+                    <img height="15px" src={collapse}/>
+                </div>
+
+                <hr />
+                <img src={this.state.preview} height={this.state.scale[0]} width={this.state.scale[1]}></img>
+                <div id={this.state.id + "Desc"}>
+                    <p>{this.state.desc}</p>
+                </div>
+                <br />
+                <div> <a href={this.state.link}>Try the demo here!</a> </div>
+
+            </div>);
+    }
+}
+
+export default ProjectElement;
